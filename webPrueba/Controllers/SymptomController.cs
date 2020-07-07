1df065fbc854b81d671e8abc4c4a63cae77d02f2
paolo.martinez <paolo.martinez@dolmencg.com>
2020-07-07 17:34:06 -0500

    Catalogo de Sintomas

---
+++ webPrueba/Controllers/SymptomController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webPrueba.DataAccess.Catalogs;
+using webPrueba.Models;
+
+namespace webPrueba.Controllers
+{
+    public class SymptomController : Controller
+    {
+        // GET: Symptom
+        public ActionResult Index()
+        {
+            List<SymptomVM> lvm = new List<SymptomVM>();
+            lvm = daSymptom.GetSymptomsAll();
+            return View(lvm);
+        }
+
+        public ActionResult Edit(string Id)
+        {
+
+            SymptomVM vm = new SymptomVM();
+            vm = daSymptom.GetSymptomsById(int.Parse(Id));
+            return View(vm);
+        }
+    }
+}
---
+++ webPrueba/Models/SymptomVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webPrueba.Models
+{
+    public class SymptomVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string SymptomName { get; set; }
+
+        [Required]
+        [Display(Name = "Descripción")]
+        public string SynptomDescription { get; set; }
+
+        [Display(Name = "URL Imagen")]
+        public string URLImage { get; set; }
+
+        [Display(Name = "Medida Mínima")]
+        public decimal Measure { get; set; }
+
+        [Display(Name = "Activo")]
+        public bool Active { get; set; }
+    }
+
+}
